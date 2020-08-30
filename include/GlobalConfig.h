c6a6f34fb4a49ef5a254a4d8f71001e13d233a9e
BOJIT <35117353+BOJIT@users.noreply.github.com>
2020-08-30 10:08:23 +0100

    Use correct git submodules

---
+++ include/GlobalConfig.h
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * @brief Global application configuration
+ *
+ * @author @htmlonly &copy; @endhtmlonly 2020 James Bennion-Pedley
+ *
+ * @date 7 June 2020
+ */
+
+#ifndef __GLOBAL_CONFIG__
+#define __GLOBAL_CONFIG__
+
+/*----------------------------- General Config -------------------------------*/
+
+/// Enables all system debugging features if defined
+#define DEBUG
+
+/// Macro for number of elements in an array
+///#define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
+
+/* Clock configuration */
+#define HSE_FREQ         8000000    ///< MCU input oscillator frequency
+#define SYSCLK_FREQ      96000000   ///< Core MCU system frequency
+
+
+/*----------------------------- FreeRTOS Config ------------------------------*/
+
+#define FREERTOS_PRIORITIES     10  ///< Number of priorities that FreeRTOS uses
+
+/*----------------------------------------------------------------------------*/
+
+#endif /* __GLOBAL_CONFIG__ */
